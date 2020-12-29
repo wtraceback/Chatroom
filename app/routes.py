69a6473a586f3f48f7e9120cd9423cc2c1fa816b
wtraceback <whxcer@outlook.com>
2020-12-29 21:33:22 +0800

    使用 flask-wtf 制作登录表单

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
-from flask import render_template
+from flask import render_template, redirect
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 def index():
     title = 'Chatroom'
-    
+
     return render_template('index.html', title=title)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/')
+
+    return render_template('login.html', title='Sign In', form=form)
