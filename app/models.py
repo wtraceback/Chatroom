6a5ea499c1d5d94d02caff96913a00ff7f0c4c76
wtraceback <whxcer@outlook.com>
2020-12-30 19:29:50 +0800

    使用 SQLAlchemy 操作数据库以及添加相关的配置

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    messages = db.relationship('Message', back_populates='author', cascade='all')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    author = db.relationship('User', back_populates='messages')
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
--- config.py
+++ config.py
@@ -1,5 +1,16 @@
 import os
+import sys
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', prefix + os.path.join(basedir, 'data.db'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
