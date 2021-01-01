3c6f8ed26ccfbca2970bb5c460180c608dec4c5a
wtraceback <whxcer@outlook.com>
2021-01-01 12:38:13 +0800

    使用程序工厂函数

--- app/__init__.py
+++ app/__init__.py
@@ -5,20 +5,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 
-app = Flask(__name__)
-config_name = os.getenv('FLASK_CONFIG', 'default')
-app.config.from_object(config[config_name])
-config[config_name].init_app(app)
-db = SQLAlchemy(app)
-login = LoginManager(app)
+db = SQLAlchemy()
+login = LoginManager()
 
 
-from app.auth import auth_bp
-app.register_blueprint(auth_bp)
-from app.chat import chat_bp
-app.register_blueprint(chat_bp)
-from app.errors import errors_bp
-app.register_blueprint(errors_bp)
+def create_app(config_name=None):
+    if config_name is None:
+        config_name = os.getenv('FLASK_CONFIG', 'default')
+
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    register_extensions(app)
+    register_blueprints(app)
+
+    return app
 
 
 login.login_view = 'auth.login'
@@ -30,4 +32,15 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-from app import models
+def register_extensions(app):
+    db.init_app(app)
+    login.init_app(app)
+
+
+def register_blueprints(app):
+    from app.auth import auth_bp
+    app.register_blueprint(auth_bp)
+    from app.chat import chat_bp
+    app.register_blueprint(chat_bp)
+    from app.errors import errors_bp
+    app.register_blueprint(errors_bp)
