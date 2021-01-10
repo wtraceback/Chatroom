acc5b81a94e01a52ead1287732a832a6036cef7c
wtraceback <whxcer@outlook.com>
2021-01-10 23:31:01 +0800

    完善画面：更换头像为怪物头像、添加项目作者、进入画面，消息定位到最底下（即最新消息）、默认记住密码

--- app/models.py
+++ app/models.py
@@ -23,7 +23,7 @@ class User(UserMixin, db.Model):
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+        return 'https://www.gravatar.com/avatar/{}?d=monsterid&s={}'.format(digest, size)
 
 
 class Message(db.Model):
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
-    $('.message .close').on('click', function() {
-        $(this).closest('.message').transition('fade');
-    });
+    var activateSemantics = function() {
+        $('.message .close').on('click', function() {
+            $(this).closest('.message').transition('fade');
+        });
 
-    $('#toggle-sidebar').click(function() {
-        $('.menu.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
-    })
+        $('#toggle-sidebar').click(function() {
+            $('.menu.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('toggle');
+        })
+    }
+
+    var scrollToBottom = function() {
+        var $messages = $('.messages');
+        if ($messages[0] != undefined) {
+            $messages.scrollTop($messages[0].scrollHeight)
+        }
+    }
+
+    var __main = function() {
+        activateSemantics()
+        scrollToBottom()
+    }
+
+    __main()
 });
