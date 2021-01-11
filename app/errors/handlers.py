511290da5a25fb736009318a415192202093a65b
wtraceback <whxcer@outlook.com>
2021-01-11 20:48:27 +0800

    完善画面3：更改报错的信息页面

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -6,20 +6,20 @@ from app.errors import errors_bp
 
 @errors_bp.app_errorhandler(400)
 def bad_request(error):
-    return render_template('errors/400.html'), 400
+    return render_template('errors/errors.html', description=error.description, code=error.code), 400
 
 
 @errors_bp.app_errorhandler(404)
 def page_not_found(error):
-    return render_template('errors/404.html'), 404
+    return render_template('errors/errors.html', description=error.description, code=error.code), 404
 
 
 @errors_bp.app_errorhandler(500)
 def internal_server_error(error):
     db.session.rollback()
-    return render_template('errors/500.html'), 500
+    return render_template('errors/errors.html', description='Internal Server Error', code=error.code), 500
 
 
 @errors_bp.app_errorhandler(CSRFError)
 def handle_csrf_error(error):
-    return render_template('errors/400.html', description=error.description), 400
+    return render_template('errors/errors.html', description=error.description, code=error.code), 400
