9ddede41862c6358cd3326692b48c8c0a4f73c3b
wtraceback <whxcer@outlook.com>
2021-02-05 21:55:49 +0800

    代码语法高亮（pygments）

--- app/utils.py
+++ app/utils.py
@@ -3,12 +3,13 @@ from bleach import clean, linkify
 
 
 def to_html(raw):
+    extensions = ['markdown.extensions.fenced_code', 'markdown.extensions.codehilite']
     allowed_tags = ['a', 'abbr', 'b', 'br', 'blockquote', 'code', 'del', 'div',
                     'em', 'i', 'ul', 'li', 'ol', 'pre', 'strong', 'ul', 'h1',
                     'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'span', 'img']
     allowed_attributes = ['class', 'src', 'title', 'alt', 'href']
 
-    html = markdown(raw, output_format='html')
+    html = markdown(raw, output_format='html', extensions=extensions)
     clean_html = clean(html, tags=allowed_tags, attributes=allowed_attributes)
 
     return linkify(clean_html)
